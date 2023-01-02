03452b9d720b68de7aae4dba765ecae3179b8210
Kyle <kdmbus21@gmail.com>
2023-01-01 22:25:58 -0600

    add header and refactor main method.

--- HOST_ENUMERATION.cpp
+++ HOST_ENUMERATION.cpp
@@ -1,17 +1,25 @@
+//****************************************************************************************************
+//
+//       Name:         Kyle McColgan
+//       File name:    HOST_ENUMERATION.cpp
+//       Date:         1 January 2023
+//       Description: 
+//               This program contains the main class which uses pcap API for local hosts.
+//
+//****************************************************************************************************
+
 #include <iostream>
 #include <pcap/pcap.h>
 using namespace std;
 
+//****************************************************************************************************
+
 int main ( )
 {
-    cout << "Hello World!" << endl << endl << endl;
-    
-
     char *dev;
 	char errorMsg [PCAP_ERRBUF_SIZE];
 	pcap_if_t * deviceList;
 
-	//pcap_findalldevs(&deviceList, errorMsg);
 	if (pcap_findalldevs(&deviceList, errorMsg) == -1 )
 	{
 		fprintf(stderr, "Couldn't find default device: %s\n", errorMsg);
@@ -19,12 +27,6 @@ int main ( )
 	}
 	printf("Device: %s\n", dev);
 	return(0);
-
-
-    
-
-
-
-
-    return 0;
 }
+
+//****************************************************************************************************
