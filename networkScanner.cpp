b641a67005ef8b49e783a31f1adc7a251e97bc21
Kyle McColgan <kdmbus21@gmail.com>
2024-06-03 20:35:10 -0500

    Add some standard output for debugging purposes.

--- networkScanner.cpp
+++ networkScanner.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <pcap/pcap.h>
+#include <netinet/ip.h>
 using namespace std;
 
 pcap_t * session;
@@ -7,12 +8,33 @@ pcap_t * session;
 void openNetworkInterface()
 {
     char errors [PCAP_ERRBUF_SIZE];
+        cout << "\n***Opening session..." << endl;
     session = pcap_open_live("enp34s0", BUFSIZ, 1, 1000, errors);
 
     if(session == NULL)
     {
-        cerr << "Error opening the NIC: " << errors << endl;
+        cout << "Error opening the NIC: " << errors << endl;
     }
+}
+
+void extractDeviceInfo(const u_char * packet)
+{
+    //Extract device information from the packet...(IPs, MACs)
+
+    //Assuming IPv4 packet structure...
+    // struct ip *ip_header = (struct ip*) (packet + SIZE_ETHERNET); //Assuming Ethernet frame...
+    struct ip *ip_header = (struct ip*) (packet + 14); //Assuming Ethernet frame...
+
+    //Extract source and destination IP addresses...
+    char source[INET_ADDRSTRLEN];
+    char destination[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(ip_header->ip_src), source, INET_ADDRSTRLEN);
+    inet_ntop(AF_INET, &(ip_header->ip_dst), destination, INET_ADDRSTRLEN);
+
+    //Print the extracted IP addresses...
+    cout << "Source IP: " << source << endl;
+    cout << "Destination IP: " << destination << endl;
+
 }
 
 void capturePackets()
@@ -21,14 +43,21 @@ void capturePackets()
     const u_char * packet;
 
     //Capture a single packet
-    packet = pcap_next(session, &header);
+    cout << "\n***Capturing..." << endl;
+    while (true)
+    {
+        packet = pcap_next(session, &header);
+        if(packet == NULL)
+        {
+            continue;
+        }
+    }
+
     //Process the captured packet...
+    extractDeviceInfo(packet);
 }
 
-void extractDeviceInfo(const u_char * packet)
-{
-    //Extract device information from the packet...(IPs, MACs)
-}
+
 
 int main()
 {
