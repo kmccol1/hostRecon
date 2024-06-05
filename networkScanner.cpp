26eb33e336b664978db8fda8d39ab11c7e00148c
Kyle McColgan <kdmbus21@gmail.com>
2024-06-04 19:24:52 -0500

    Moved the hostList into main().

--- networkScanner.cpp
+++ networkScanner.cpp
@@ -15,13 +15,12 @@ using namespace std;
 
 //****************************************************************************************
 
-const int MAX_HOSTS=10;
+const int MAX_HOSTS=50;
 pcap_t * session;
-char hostList[MAX_HOSTS][16]; //Assuming each IP addr is stored in a 16-character array.
 
 //****************************************************************************************
 
-void displayHostList()
+void displayHostList(char (*hostList)[16], int maxLength)
 {
     cout << "Printing the host list..." << endl;
 
@@ -72,8 +71,8 @@ void extractDeviceInfo(const u_char * packet, char (&source)[16], char(&destinat
     // cout << "Source IP: " << sourceIP << endl;
     // cout << "Destination IP: " << destinationIP << endl;
 
-    cout << "Copying..." << endl;
-    cout << "Before source: " << sourceIP << endl;
+    //cout << "Copying..." << endl;
+    //cout << "Before source: " << sourceIP << endl;
     // strcpy(source, sourceIP);
     // strcpy(destination, destinationIP);
     //
@@ -94,7 +93,7 @@ void extractDeviceInfo(const u_char * packet, char (&source)[16], char(&destinat
 
 //****************************************************************************************
 
-void capturePackets()
+void capturePackets(char (*hostList)[16], int maxLength)
 {
     //Process the captured packet...
     int totalPackets = 0;
@@ -159,6 +158,7 @@ void capturePackets()
         {
             //Handle the case when the hostList is full...
             cout << "Host list is full. Cannot add more hosts." << endl;
+            break;
         }
     }
 }
@@ -167,10 +167,11 @@ void capturePackets()
 
 int main()
 {
+    char hostList[MAX_HOSTS][16]; //Assuming each IP addr is stored in a 16-character array.
     openNetworkInterface();
-    capturePackets();
+    capturePackets(hostList, MAX_HOSTS);
     //Further processing and analysis...
-    displayHostList();
+    displayHostList(hostList, MAX_HOSTS);
     return 0;
 }
 
@@ -179,54 +180,163 @@ int main()
 /*
 g++ networkScanner.cpp -lpcap -o networkScanner
 sudo ./networkScanner
-[sudo] password for root:
 
 ***Opening session...
 
 ***Capturing...
-Copying...
-Before source: 142.32.34.241
+Copied source: 192.168.1.235
+Captured source info: 192.168.1.235
 Copied source: 142.32.34.241
 Captured source info: 142.32.34.241
-Copying...
-Before source: 34.241.192.168
-Copied source: 34.241.192.168
-Captured source info: 34.241.192.168
-Copying...
-Before source: 182.116.111.98
-Copied source: 182.116.111.98
-Captured source info: 182.116.111.98
-Copying...
-Before source: 192.168.1.208
+Copied source: 2.200.100.52
+Captured source info: 2.200.100.52
+Copied source: 29.15.1.10
+Captured source info: 29.15.1.10
+Copied source: 0.0.0.0
+Captured source info: 0.0.0.0
+Copied source: 0.0.0.0
+Captured source info: 0.0.0.0
+Copied source: 0.0.0.0
+Captured source info: 0.0.0.0
+Copied source: 0.0.0.0
+Captured source info: 0.0.0.0
+Copied source: 0.0.0.0
+Captured source info: 0.0.0.0
+Copied source: 0.0.0.0
+Captured source info: 0.0.0.0
+Copied source: 0.0.0.0
+Captured source info: 0.0.0.0
+Copied source: 0.0.0.0
+Captured source info: 0.0.0.0
+Copied source: 0.0.0.0
+Captured source info: 0.0.0.0
+Copied source: 0.0.0.0
+Captured source info: 0.0.0.0
+Copied source: 167.208.92.32
+Captured source info: 167.208.92.32
+Copied source: 0.0.0.0
+Captured source info: 0.0.0.0
+Copied source: 142.32.34.241
+Captured source info: 142.32.34.241
+Copied source: 192.168.1.235
+Captured source info: 192.168.1.235
+Copied source: 2.200.100.52
+Captured source info: 2.200.100.52
+Copied source: 142.32.34.241
+Captured source info: 142.32.34.241
+Copied source: 29.15.1.10
+Captured source info: 29.15.1.10
+Copied source: 2.200.100.52
+Captured source info: 2.200.100.52
+Copied source: 192.168.1.235
+Captured source info: 192.168.1.235
+Copied source: 142.32.34.241
+Captured source info: 142.32.34.241
+Copied source: 192.168.1.235
+Captured source info: 192.168.1.235
+Copied source: 2.200.100.52
+Captured source info: 2.200.100.52
+Copied source: 142.32.34.241
+Captured source info: 142.32.34.241
+Copied source: 29.15.1.10
+Captured source info: 29.15.1.10
+Copied source: 104.18.37.204
+Captured source info: 104.18.37.204
 Copied source: 192.168.1.208
 Captured source info: 192.168.1.208
-Copying...
-Before source: 192.168.1.208
+Copied source: 2.200.100.52
+Captured source info: 2.200.100.52
+Copied source: 142.32.34.241
+Captured source info: 142.32.34.241
+Copied source: 104.18.37.204
+Captured source info: 104.18.37.204
 Copied source: 192.168.1.208
 Captured source info: 192.168.1.208
-Copying...
-Before source: 192.168.1.254
-Copied source: 192.168.1.254
-Captured source info: 192.168.1.254
-Copying...
-Before source: 192.168.1.254
-Copied source: 192.168.1.254
-Captured source info: 192.168.1.254
-Copying...
-Before source: 167.208.92.32
+Copied source: 192.168.1.235
+Captured source info: 192.168.1.235
 Copied source: 167.208.92.32
 Captured source info: 167.208.92.32
-Copying...
-Before source: 167.208.92.32
+Copied source: 0.1.5.16
+Captured source info: 0.1.5.16
+Copied source: 0.1.5.16
+Captured source info: 0.1.5.16
 Copied source: 167.208.92.32
 Captured source info: 167.208.92.32
-Copying...
-Before source: 167.208.92.32
-Copied source: 167.208.92.32
-Captured source info: 167.208.92.32
-Copying...
-Before source: 167.208.92.32
-Copied source: 167.208.92.32
-Captured source info: 167.208.92.32
+Copied source: 2.200.100.52
+Captured source info: 2.200.100.52
+Copied source: 192.168.1.235
+Captured source info: 192.168.1.235
+Copied source: 142.32.34.241
+Captured source info: 142.32.34.241
+Copied source: 29.15.1.10
+Captured source info: 29.15.1.10
+Copied source: 2.200.100.52
+Captured source info: 2.200.100.52
+Copied source: 142.32.34.241
+Captured source info: 142.32.34.241
+Copied source: 192.168.1.235
+Captured source info: 192.168.1.235
+Copied source: 192.168.1.208
+Captured source info: 192.168.1.208
+Copied source: 2.200.100.52
+Captured source info: 2.200.100.52
+Copied source: 152.67.232.7
+Captured source info: 152.67.232.7
 Host list is full. Cannot add more hosts.
+Printing the host list...
+Host 1: �d@��
+����
+Host 2: u����c
+Host 3: 49.57.50.46�
+Host 4: 49.52.50.46h�L�
+Host 5: 50.46.50.48�
+Host 6: 50.57.46.49�
+Host 7: 48.46.48.46�
+Host 8: 48.46.48.46�
+Host 9: 48.46.48.46
+Host 10: 48.46.48.46
+Host 11: 48.46.48.46
+Host 12: 48.46.48.46�
+Host 13: 48.46.48.46�
+Host 14: 48.46.48.46
+Host 15: 48.46.48.46
+Host 16: 48.46.48.46
+Host 17: 49.54.55.46@
+Host 18: 48.46.48.46
+Host 19: 49.52.50.46@
+Host 20: 49.57.50.46@
+Host 21: 50.46.50.48@
+Host 22: 49.52.50.46@
+Host 23: 50.57.46.49@
+Host 24: 50.46.50.48@
+Host 25: 49.57.50.46h�L�
+Host 26: 49.52.50.46�
+Host 27: 49.57.50.46�
+Host 28: 50.46.50.48�
+Host 29: 49.52.50.46�
+Host 30: 50.57.46.49�
+Host 31: 49.48.52.46�
+Host 32: 49.57.50.46
+Host 33: 50.46.50.48�
+Host 34: 49.52.50.46�
+Host 35: 49.48.52.46
+Host 36: 49.57.50.46�
+Host 37: 49.57.50.46h�L�
+Host 38: 49.54.55.46
+Host 39: 48.46.49.46
+Host 40: 48.46.49.46
+Host 41: 49.54.55.46�
+Host 42: 50.46.50.48
+Host 43: 49.57.50.46�
+Host 44: 49.52.50.46
+Host 45: 50.57.46.49����
+Host 46: 50.46.50.48�
+Host 47: 49.52.50.46����
+Host 48: 49.57.50.46�
+Host 49: 49.57.50.46�
+Host 50: 50.46.50.48
+
+*****************************************
+
+Done.
 */
